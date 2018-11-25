450a48cec9078edc6315bd21f85a800921553210
idabrows <idabrows@student.agh.edu.pl>
2018-11-25 16:44:39 +0100

    Comment

--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -1,14 +1,66 @@
 package model;
-import com.quantego.clp.*;
+import ilog.concert.IloException;
+import ilog.concert.IloLinearNumExpr;
+import ilog.concert.IloNumVar;
+import ilog.concert.IloRange;
+import ilog.cplex.*;
 
-public class Calculator{
+import java.util.ArrayList;
+import java.util.List;
 
-    public void calculate(){
-        CLP model = new CLP();
-        CLPVariable x = model.addVariable().lb(-3).ub(3);
-        CLPVariable y = model.addVariable().free();
-        model.createExpression().add(3,x).add(4,y).leq(10);
-        model.printModel();
-    }
 
+public class Calculator {
+        public static void solveMe() {
+            try {
+                IloCplex cplex = new IloCplex();
+
+                // variables
+                IloNumVar x = cplex.numVar(0, Double.MAX_VALUE, "x");
+                IloNumVar y = cplex.numVar(0, Double.MAX_VALUE, "y");
+
+                // expressions
+                IloLinearNumExpr objective = cplex.linearNumExpr();
+                objective.addTerm(0.12, x);
+                objective.addTerm(0.15, y);
+
+                // define objective
+                cplex.addMinimize(objective);
+
+                // define constraints
+                List<IloRange> constraints = new ArrayList<IloRange>();
+                constraints.add(cplex.addGe(cplex.sum(cplex.prod(60, x),cplex.prod(60, y)), 300));
+                constraints.add(cplex.addGe(cplex.sum(cplex.prod(12, x),cplex.prod(6, y)), 36));
+                constraints.add(cplex.addGe(cplex.sum(cplex.prod(10, x),cplex.prod(30, y)), 90));
+                IloLinearNumExpr num_expr = cplex.linearNumExpr();
+                num_expr.addTerm(2, x);
+                num_expr.addTerm(-1,y);
+                constraints.add(cplex.addEq(num_expr, 0));
+                num_expr = cplex.linearNumExpr();
+                num_expr.addTerm(1,y);
+                num_expr.addTerm(-1,x);
+                constraints.add(cplex.addLe(num_expr,8));
+
+                // display option
+                cplex.setParam(IloCplex.Param.Simplex.Display, 0);
+
+                // solve
+                if (cplex.solve()) {
+                    System.out.println("obj = "+cplex.getObjValue());
+                    System.out.println("x   = "+cplex.getValue(x));
+                    System.out.println("y   = "+cplex.getValue(y));
+                    for (int i=0;i<constraints.size();i++) {
+                        System.out.println("dual constraint "+(i+1)+"  = "+cplex.getDual(constraints.get(i)));
+                        System.out.println("slack constraint "+(i+1)+" = "+cplex.getSlack(constraints.get(i)));
+                    }
+                }
+                else {
+                    System.out.println("Model not solved");
+                }
+
+                cplex.end();
+            }
+            catch (IloException exc) {
+                exc.printStackTrace();
+            }
+        }
 }
--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,13 +1,14 @@
 package view;
 import model.Calculator;
 
+
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Calculator c=new Calculator();
-        c.calculate();
+        c.solveMe();
 
     }
 }
